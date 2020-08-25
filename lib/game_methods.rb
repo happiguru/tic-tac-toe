628a975e2eae78b8adbb33be846510c427308334
Jose <pastorpedraza3@gmail.com>
2020-08-25 10:13:46 -0500

    add review changes

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -3,8 +3,10 @@
 # rubocop:disable Metrics/PerceivedComplexity
 
 class Game
-  def initialize
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  attr_reader :board
+  attr_writer :board
+  def initialize(input)
+    @board = input
   end
 
   def display_board
@@ -17,41 +19,45 @@ class Game
     puts "\n"
   end
 
+  def user_input_v(user_input,board)
+    if user_input.is_a?(Integer) && (1..9).include?(user_input)
+      valid = true
+      user_input = Integer(user_input)
+      if board.include?(user_input)
+        board.delete(user_input)
+        return valid = true
+      else
+        puts 'Position givn already used!!!!'
+        return valid = false
+      end
+    else
+      return valid = false
+      puts 'No valid value!!'
+    end
+  end
+
   def user_input(board, npos)
     valid = false
     while valid == false
       puts 'Please enter value between 1-9'
       n = String(npos)
-      if n.match(/[0,2,4,6,9]/)
+      if n.match(/[0,2,4,6,8]/)
         puts 'Player 1:'
-      else
+      elsif n.match(/[1,3,5.7]/)
         puts 'Player 2:'
       end
       user_input = gets.chomp
       user_input = Integer(user_input) if user_input.match(/[a-z]/).nil?
-      if user_input.is_a?(Integer) && (1..9).include?(user_input)
-        valid = true
-        user_input = Integer(user_input)
-        if board.include?(user_input)
-          board.delete(user_input)
-          user_input -= 1
-          valid = true
-        else
-          puts 'Position givn already used!!!!'
-          valid = false
-        end
-      else
-        valid = false
-        puts 'No valid value!!'
-      end
+      valid = user_input_v(user_input,board)
     end
+    user_input -= 1
     @chosen_index = user_input
     @chosen_index
   end
 
   def update_board(board, npos)
     n = String(npos)
-    board[@chosen_index] = if n.match(/[0,2,4,6,9]/)
+    board[@chosen_index] = if n.match(/[0,2,4,6,8]/)
                              'X'
                            else
                              'O'
@@ -67,8 +73,12 @@ class Game
 end
 
 class Winner
-  def initialize(user_input)
+    attr_reader :user_input
+    attr_writer :user_input
+    attr_reader :win
+  def initialize(user_input,win)
     @input = user_input
+    @win = win
   end
 
   def board(npos, p1c, p2c)
@@ -81,6 +91,7 @@ class Winner
     [p1c, p2c]
   end
 end
+
 
 # rubocop:enable Metrics/PerceivedComplexity
 # rubocop:enable Metrics/CyclomaticComplexity
